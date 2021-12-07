7aca928c8781219672263dcf1d6e6c81897585ca
LaszloGulyas <laszlogulyas.jr@gmail.com>
2021-12-07 15:41:28 +0100

    Database connection set, create default table script added

---
+++ Sql_Scripts/create_table_Tasks.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE Tasks
+(
+    [TaskID] INT IDENTITY(1,1) PRIMARY KEY, 
+    [TaskName] NVARCHAR(50) NULL, 
+    [TaskDescription] NVARCHAR(500) NULL, 
+    [TaskDateOfCreation] DATETIME NULL, 
+    [TaskDueDate] DATETIME NULL, 
+    [TaskIsCompleted] BIT NULL
+)
